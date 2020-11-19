0d9d6d6deb8bd2e5130c9fc06ad58ef45669d9e8
sheldmel <dmello.shelton@yahoo.com>
2020-11-19 17:12:47 -0500

    added view profile and edit profile

---
+++ app/src/main/java/ca/gbc/comp3074/crms/ViewProfile.java
@@ -0,0 +1,28 @@
+package ca.gbc.comp3074.crms;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProfile extends AppCompatActivity {
+    private Button editProfile_button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_profile);
+        editProfile_button = (Button) findViewById(R.id.button_editProfile);
+        editProfile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditProfile();
+            }
+        });
+    }
+    public void openEditProfile(){
+        Intent intent = new Intent(this, EditProfile.class);
+        startActivity(intent);
+    }
+}
