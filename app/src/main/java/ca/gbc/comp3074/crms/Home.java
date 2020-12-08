a9b764065d0af9419350cfd971e36eed46afa5a7
linminca <min-lin@hotmail.com>
2020-12-08 18:03:04 -0500

    update restaurant ListView

--- app/src/main/java/ca/gbc/comp3074/crms/Home.java
+++ app/src/main/java/ca/gbc/comp3074/crms/Home.java
@@ -1,24 +1,51 @@
 package ca.gbc.comp3074.crms;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.os.IResultReceiver;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ca.gbc.comp3074.crms.models.Restaurant;
 
 public class Home extends AppCompatActivity {
 
     Button logoutBtn;
     TextView currentUserText;
 
+    ListView myListView;
+    List<Restaurant> restaurantList;
+
+    DatabaseReference restaurantRef;
     @Override
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,15 +98,36 @@ public class Home extends AppCompatActivity {
         Intent intent = getIntent();
         FirebaseUser currentUser = (FirebaseUser) intent.getExtras().get("currentUser");
 
-//        currentUserText.setText("USER: " + currentUser.getEmail() + ", " + currentUser.getUid());
-        currentUserText.setText("USER: " + currentUser.getEmail() );
+//        currentUserText.setText("Welcome: " + currentUser.getEmail() );
 
         logoutBtn.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
             Intent intent1 = new Intent(Home.this, LoginActivity.class);
             startActivity(intent1);
         });
+
+        myListView = findViewById(R.id.myListView);
+        restaurantList = new ArrayList<>();
+        FirebaseFirestore resRef = FirebaseFirestore.getInstance();
+        CollectionReference questionsRef = resRef.collection("restaurants");
+        questionsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        Restaurant restaurant = document.toObject(Restaurant.class);
+                        restaurantList.add(restaurant);
+                        Log.d("TAG", restaurant.getName());
+                    }
+                    ListAdapter adapter = new ListAdapter(Home.this, restaurantList);
+                    myListView.setAdapter(adapter);
+
+                }else{
+                    Log.d("error","not working");
+                }
+            }
+        });
+
+
     }
-
-
 }
---
+++ app/src/main/java/ca/gbc/comp3074/crms/ListAdapter.java
@@ -0,0 +1,70 @@
+package ca.gbc.comp3074.crms;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import ca.gbc.comp3074.crms.models.Restaurant;
+
+public class ListAdapter extends ArrayAdapter {
+
+    private Activity mContext;
+    List<Restaurant> restaurantList;
+
+    public ListAdapter(Activity mContext, List<Restaurant> restaurantList){
+        super(mContext, R.layout.list_item, restaurantList );
+        this.mContext = mContext;
+        this.restaurantList = restaurantList;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = mContext.getLayoutInflater();
+        View listItemView = inflater.inflate(R.layout.list_item,null,true);
+
+        TextView resName = listItemView.findViewById(R.id.restaurantName);
+        TextView resAddress = listItemView.findViewById(R.id.restaurantAddress);
+        ImageView resImage = listItemView.findViewById(R.id.rImageView);
+        RatingBar resRating = listItemView.findViewById(R.id.restaurantRating);
+        Button btnBooking = listItemView.findViewById(R.id.booking);
+
+        Restaurant restaurant = restaurantList.get(position);
+
+        resName.setText(restaurant.getName());
+        resAddress.setText(restaurant.getAddress());
+        Picasso.get().load(restaurant.getImage()). into(resImage);
+        resRating.setRating(restaurant.getRating().floatValue());
+
+        btnBooking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.startActivity(new Intent(mContext,MakeReservation.class));
+                Log.d("click","clicked");
+            }
+        });
+
+        return listItemView;
+    }
+
+
+}
